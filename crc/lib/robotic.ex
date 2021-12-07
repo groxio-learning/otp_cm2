995de7c6052c04acacb788e2990bf70b1cb89ac3
Osman Medzhytov <osman.medzhytov@caremore.com>
2021-12-07 15:28:28 -0500

    RObot turn staff

--- crc/lib/robotic.ex
+++ crc/lib/robotic.ex
@@ -3,4 +3,21 @@ defmodule Robotic do
 
   def new(p, o), do: %__MODULE__{position: p, orientation: o}
 
+  def turn_left(%Robotic{orientation: orientation} = robot) do
+    case orientation do
+      :north -> %{robot | orientation: :west}
+      :south -> %{robot | orientation: :east}
+      :east -> %{robot | orientation: :north}
+      :west -> %{robot | orientation: :south}
+    end
+  end
+
+  def turn_right(%Robotic{orientation: orientation} = robot) do
+    case orientation do
+      :north -> %{robot | orientation: :east}
+      :south -> %{robot | orientation: :west}
+      :east -> %{robot | orientation: :south}
+      :west -> %{robot | orientation: :north}
+    end
+  end
 end
