0c0ed9bdda51470aa2c20972a918723dc3d13845
Jaemin Shim <jashim@caremore.com>
2021-12-08 11:02:03 -0800

    updated service for client layer

--- crc/lib/service.ex
+++ crc/lib/service.ex
@@ -1,4 +1,5 @@
 defmodule Service do
+  # Server Layer
   def start({p, o}) do
     spawn(fn -> loop(Robotic.new(p, o)) end)
   end
@@ -11,9 +12,31 @@ defmodule Service do
     receive do
       {:move, action} ->
         Robotic.move(robot, action)
+
       {:status, from} ->
         send(from, Robotic.humanize(robot))
-      robot
+        robot
+    end
+  end
+
+  #  Client Layer
+  def left(robot_pid) do
+    send(robot_pid, {:move, :left})
+  end
+
+  def right(robot_pid) do
+    send(robot_pid, {:move, :right})
+  end
+
+  def forward(robot_pid) do
+    send(robot_pid, {:move, :forward})
+  end
+
+  def status(robot_pid) do
+    send(robot_pid, {:status, self()})
+
+    receive do
+      string -> string
     end
   end
 end
