c74731f7a792c16889964aa9496239c47842ab31
Osman Medzhytov <osman.medzhytov@caremore.com>
2021-12-09 15:40:09 -0500

    Application impl

--- gc/lib/gc/application.ex
+++ gc/lib/gc/application.ex
@@ -8,8 +8,7 @@ defmodule Gc.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Gc.Worker.start_link(arg)
-      # {Gc.Worker, arg}
+      {Gc.Server, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
